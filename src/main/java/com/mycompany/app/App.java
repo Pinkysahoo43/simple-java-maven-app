529fcd4943b5b55fda2ba0e419e5f6fa83a0a019
Pinkysahoo43 <132168565+Pinkysahoo43@users.noreply.github.com>
2023-08-28 12:25:01 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -15,7 +15,7 @@ public class App {
 
     public String getMessage() {
         System.out.println("Hello");
-        System.out.println("Helloo");
+        System.out.println("Hello1");
         return MESSAGE;
     }
 }
